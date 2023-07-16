ca2c3f3163165bde69838da5e9de6f6b269e10f8
saidaz24-meet <139681209+saidaz24-meet@users.noreply.github.com>
2023-07-17 00:19:59 +0300

    Update review.py

--- review.py
+++ review.py
@@ -1,5 +1,5 @@
-def create_youtube_video(title,description) :
-	newvideo = {"title" : title,"description" : description,"likes" : 0,"dislikes" : 0,"comments" : {}}
+def create_youtube_video(title,description,hashtag) :
+	newvideo = {"title" : title,"description" : description,"likes" : 0,"dislikes" : 0,"comments" : {}, "hashtag" : hashtag}
 	return newvideo
 
 def like (youtube_vid):
@@ -19,8 +19,23 @@ def add_coment(youtubevideo,username,comment_text):
 
 	 return youtubevideo
 
+def similarity_to_video(youtubevideo1,youtubevideo2):
+	num = len(youtubevideo1)
+	similar = 0
+	for i in range (num):
+		if youtubevideo1[i] == youtubevideo2[i] :
+			similar = similar +1
 
- create_youtube_video("first review","nothing")
+	return similar/num
+
+
+
+
+n = create_youtube_video("first review","nothing",["amazing","wow","les go","nice","genuis"])
+for i in range (494) :
+	like(n)
+print(n) 
+
 
 
 	
